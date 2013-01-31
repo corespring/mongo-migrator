7f444950cd8a7b812cce681f0b0d0efde3074701
edeustace <ed.eustace@gmail.com>
2013-01-31 11:07:57 +0100

    allow option to pass in db uri

--- src/test/scala/org/corespring/migrator/helpers/DbSingleton.scala
+++ src/test/scala/org/corespring/migrator/helpers/DbSingleton.scala
@@ -1,11 +1,24 @@
 package org.corespring.migrator.helpers
 
 import com.mongodb.casbah.{MongoURI, MongoDB, MongoConnection}
+import org.corespring.migrator.models.DbName
 
 object DbSingleton {
   lazy val connection : MongoConnection = MongoConnection(MongoURI(mongoUri))
-  lazy val db : MongoDB = connection(dbName)
+  lazy val db : MongoDB = init
+
   val dbName = "mongo_migrator_test_db"
-  val mongoUri = "mongodb://localhost:27017/" + dbName
+  val envUri = System.getenv("MONGO_MIGRATOR_TEST_DB_URI")
+  val mongoUri = if(envUri == null) "mongodb://localhost:27017/" + dbName else envUri
+
+  private def init : MongoDB = {
+    val db = connection(dbName)
+    val name = DbName(mongoUri)
+    println("db -> " + name + " uri: " + mongoUri)
+    name.username.map { u =>
+      db.authenticate(u, name.password.get)
+    }
+    db
+  }
 
 }
--- src/test/scala/org/corespring/migrator/tests/Cleanup.scala
+++ src/test/scala/org/corespring/migrator/tests/Cleanup.scala
@@ -2,9 +2,10 @@ package org.corespring.migrator.tests
 
 import org.corespring.migrator.helpers.DbSingleton
 import org.corespring.migrator.log.Logger
+import com.mongodb.casbah.commons.MongoDBObject
 
 class Cleanup {
   Logger.get("Cleanup").info("do cleanup")
-  DbSingleton.db.dropDatabase()
+  DbSingleton.db.getCollection("mongo_migrator_versions").remove(MongoDBObject())
   DbSingleton.connection.close()
 }
