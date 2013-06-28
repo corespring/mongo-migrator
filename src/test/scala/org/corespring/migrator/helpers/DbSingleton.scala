b5719883f9ceabc98c7137695738132e6c14e706
edeustace <ed.eustace@gmail.com>
2013-06-28 12:04:11 +0100

    update DbSingleton to use the DbHelper

--- src/test/scala/org/corespring/migrator/helpers/DbSingleton.scala
+++ src/test/scala/org/corespring/migrator/helpers/DbSingleton.scala
@@ -1,24 +1,13 @@
 package org.corespring.migrator.helpers
 
-import com.mongodb.casbah.{MongoURI, MongoDB, MongoConnection}
-import org.corespring.migrator.models.DbInfo
+import com.mongodb.casbah.{MongoDB, MongoConnection}
+import org.corespring.migrator.models.DbHelper
 
 object DbSingleton {
-  lazy val connection : MongoConnection = MongoConnection(MongoURI(mongoUri))
-  lazy val db : MongoDB = init
-
   val dbName = "mongo_migrator_test_db"
-  val envUri = System.getenv("MONGO_MIGRATOR_TEST_DB_URI")
-  val mongoUri = if(envUri == null) "mongodb://localhost:27017/" + dbName else envUri
-
-  private def init : MongoDB = {
-    val db = connection(dbName)
-    val name = DbInfo(mongoUri)
-    println("db -> " + name + " uri: " + mongoUri)
-    name.username.map { u =>
-      db.authenticate(u, name.password.get)
-    }
-    db
-  }
-
+  lazy val envUri = System.getenv("MONGO_MIGRATOR_TEST_DB_URI")
+  lazy val mongoUri = if(envUri == null) "mongodb://localhost:27017/" + dbName else envUri
+  lazy val helper : DbHelper = new DbHelper(mongoUri)
+  lazy val connection : MongoConnection = helper.connection
+  lazy val db : MongoDB = helper.mongoDB
 }
